2b297d3c70930c1f4c9d7dd0a1496320af0c12d7
KhaiNV <KhaiNV@vietis.com.vn>
2025-08-15 17:13:39 +0700

    add source product

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(ProductModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Product API')
+    .setDescription('CRUD Product (in-memory) với Swagger')
+    .setVersion('1.0.0')
+    .addTag('products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
+  await app.listen(3000);
+  console.log(`🚀 Server chạy: http://localhost:3000`);
+  console.log(`📘 Swagger:     http://localhost:3000/api`);
 }
 bootstrap();
